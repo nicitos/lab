312f5c85f8ce064c28f7ff9e73031bb2d8865a89
nicitos <114752991+nicitos@users.noreply.github.com>
2025-03-21 08:50:46 +0300

    Update WeatherResponse.java

--- src/main/java/com/example/demo11/model/WeatherResponse.java
+++ src/main/java/com/example/demo11/model/WeatherResponse.java
@@ -1,4 +1,50 @@
-package model;
+package com.example.demo11.model;
 
 public class WeatherResponse {
+    private String city;
+    private double temperature;
+    private double windSpeed;
+    private String windDirection;
+    private String description;
+
+    // Геттеры и сеттеры
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getWindDirection() {
+        return windDirection;
+    }
+
+    public void setWindDirection(String windDirection) {
+        this.windDirection = windDirection;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
