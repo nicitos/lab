f9f587a07612c1bfe9a104d0a7c8947fda9cf1f3
стук валера <stuk.nicit@mail.ru>
2025-04-04 13:11:08 +0300

    upd

--- src/main/java/com/example/demo11/service/WeatherService.java
+++ src/main/java/com/example/demo11/service/WeatherService.java
@@ -1,34 +1,46 @@
 package com.example.demo11.service;
 
 import com.example.demo11.model.WeatherData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
+
     @Value("${openweathermap.api.key}")
     private String apiKey;
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public WeatherService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public WeatherData getWeather(String city, Double lat, Double lon) {
         String url = buildUrl(city, lat, lon);
-        return restTemplate.getForObject(url, WeatherData.class);
+        try {
+            return restTemplate.getForObject(url, WeatherData.class);
+        } catch (HttpClientErrorException e) {
+            throw new RuntimeException("Ошибка запроса к OpenWeatherMap: " + e.getMessage());
+        }
     }
 
     private String buildUrl(String city, Double lat, Double lon) {
         String baseUrl = "https://api.openweathermap.org/data/2.5/weather?appid=%s&units=metric";
-        String url;
+        StringBuilder urlBuilder = new StringBuilder();
 
         if (city != null && !city.isEmpty()) {
-            url = String.format(baseUrl + "&q=%s", apiKey, city);
+            urlBuilder.append(String.format(baseUrl + "&q=%s", apiKey, city));
         } else if (lat != null && lon != null) {
-            url = String.format(baseUrl + "&lat=%f&lon=%f", apiKey, lat, lon);
+            urlBuilder.append(String.format(baseUrl + "&lat=%f&lon=%f", apiKey, lat, lon));
         } else {
             throw new IllegalArgumentException("Укажите город или координаты.");
         }
 
-        return url;
+        return urlBuilder.toString();
     }
 }
