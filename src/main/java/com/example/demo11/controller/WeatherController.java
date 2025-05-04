a9ac754cf90c7902331050869db8387b470c16f9
nicitos <114752991+nicitos@users.noreply.github.com>
2025-05-04 21:29:18 +0300

    Update WeatherController.java

--- src/main/java/com/example/demo11/controller/WeatherController.java
+++ src/main/java/com/example/demo11/controller/WeatherController.java
@@ -2,6 +2,8 @@ package com.example.demo11.controller;
 
 import com.example.demo11.dto.WeatherData;
 import com.example.demo11.service.WeatherService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,10 +15,11 @@ public class WeatherController {
     private WeatherService weatherService;
 
     @GetMapping("/weather")
+    @Operation(summary = "Получить текущую погоду по городу или координатам")
     public WeatherData getWeather(
-            @RequestParam(required = false) String city,
-            @RequestParam(required = false) Double lat,
-            @RequestParam(required = false) Double lon
+            @RequestParam(required = false) @Parameter(description = "Название города") String city,
+            @RequestParam(required = false) @Parameter(description = "Широта") Double lat,
+            @RequestParam(required = false) @Parameter(description = "Долгота") Double lon
     ) {
         return weatherService.getWeather(city, lat, lon);
     }
